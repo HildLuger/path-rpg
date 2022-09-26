0ded75fc014749f0534b18b8ec210162a05eaafa
HildLuger <88519194+HildLuger@users.noreply.github.com>
2022-09-26 01:49:41 -0300

    Add files via upload

---
+++ rpg1 - Copy/caminhos.js
@@ -0,0 +1,115 @@
+document.getElementById("button1").onclick = function caminho1() {
+
+    document.getElementById("texto0").style.display = "none";
+    document.getElementById("texto1").style.display = "block";
+    document.getElementById("background1").style.display = "block";
+    document.getElementById("button1").style.display = "none";
+    document.getElementById("button2").style.display = "none";
+    document.getElementById("button3").style.display = "none";
+    document.getElementById("button4").style.display = "block";
+    document.getElementById("button5").style.display = "block";
+    document.getElementById("button6").style.display = "block";
+    document.getElementById("button7").style.display = "none";
+    document.getElementById("button8").style.display = "none";
+    document.getElementById("button9").style.display = "none";
+    document.getElementById("button10").style.display = "none";
+    document.getElementById("button11").style.display = "none";
+    document.getElementById("button12").style.display = "none";
+    document.getElementById("back").style.display = "block";
+
+    var audio = new Audio('/sounds/Lorem Ipsum.mp3');
+    audio.play();
+    
+
+    var audio = new Audio('/sounds/b1.mp3');
+    audio.play();
+    
+
+
+
+
+}
+
+
+document.getElementById("button2").onclick = function caminho2() {
+
+    document.getElementById("texto0").style.display = "none";
+    document.getElementById("texto1").style.display = "none";
+    document.getElementById("texto2").style.display = "block";
+    document.getElementById("background1").style.display = "none";
+    document.getElementById("background2").style.display = "block";
+    document.getElementById("button1").style.display = "none";
+    document.getElementById("button2").style.display = "none";
+    document.getElementById("button3").style.display = "none";
+    document.getElementById("button4").style.display = "none";
+    document.getElementById("button5").style.display = "none";
+    document.getElementById("button6").style.display = "none";
+    document.getElementById("button7").style.display = "block";
+    document.getElementById("button8").style.display = "block";
+    document.getElementById("button9").style.display = "block";
+    document.getElementById("back").style.display = "block";
+
+    var audio = new Audio('/sounds/Lorem Ipsum.mp3');
+    audio.play();
+    
+
+    var audio = new Audio('/sounds/b2.mp3');
+    audio.play();
+
+   
+
+}
+
+document.getElementById("button3").onclick = function caminho3() {
+
+    document.getElementById("texto0").style.display = "none";
+    document.getElementById("texto3").style.display = "block";
+    document.getElementById("background3").style.display = "block";
+    document.getElementById("button1").style.display = "none";
+    document.getElementById("button2").style.display = "none";
+    document.getElementById("button3").style.display = "none";
+    document.getElementById("button4").style.display = "none";
+    document.getElementById("button5").style.display = "none";
+    document.getElementById("button6").style.display = "none";
+    document.getElementById("button7").style.display = "none";
+    document.getElementById("button8").style.display = "none";
+    document.getElementById("button9").style.display = "none";
+    document.getElementById("button10").style.display = "block";
+    document.getElementById("button11").style.display = "block";
+    document.getElementById("button12").style.display = "block";
+    document.getElementById("back").style.display = "block";
+
+
+
+    var audio = new Audio('/sounds/Lorem Ipsum.mp3');
+    audio.play();
+    
+
+    var audio = new Audio('/sounds/b3.mp3');
+    audio.play();
+
+
+}
+
+
+
+document.getElementById("back").onclick = function back() {
+
+    document.getElementById("texto0").style.display = "block";
+    document.getElementById("button1").style.display = "block";
+    document.getElementById("button2").style.display = "block";
+    document.getElementById("button3").style.display = "block";
+    document.getElementById("button4").style.display = "block";
+    document.getElementById("button4").style.display = "none";
+    document.getElementById("button5").style.display = "none";
+    document.getElementById("button6").style.display = "none";
+    document.getElementById("background1").style.display = "none";
+    document.getElementById("back").style.display = "none";
+    document.getElementById("texto1").style.display = "none";
+    document.getElementById("texto2").style.display = "none";
+    document.getElementById("texto3").style.display = "none";
+
+    window.location.reload();
+
+}
+
---
+++ rpg1 - Copy/playAudio.js
@@ -0,0 +1,5 @@
+const audio = new Audio();
+audio.src = "/sounds/dice.mp3"; 
+
+
+
